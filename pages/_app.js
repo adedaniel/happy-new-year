41f7359e06d73def6abbc096ca61e02e12118942
adedaniel <adetoladaniel693@gmail.com>
2020-01-01 09:37:28 +0100

    remove getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,7 @@ import React from "react";
 
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
+ 
   render() {
     const { Component, pageProps } = this.props;
 
